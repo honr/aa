dbbdd40a17e5f6b38f10262ee0ee1985153cd84b
El Rey <el@rey>
2017-09-05 06:40:22 -0700

    +

--- hello/greet.cc
+++ hello/greet.cc
@@ -9,18 +9,19 @@ int f() {
 
 int main() {
   std::cout << "without digit separators: " << 123456 << '\n';
-  // std::cout << "with digit separators: " << 123'456'789 << '\n';
+  std::cout << "with digit separators: " << 123'456'789 << '\n';
   std::cout << "binary literal 5: " << 0b101 << '\n';
 
   std::string s = "foo-bar";
   std::cout << "s.substr(1): '" << s.substr(1) << "'.\n";
 
-  // Needs gcc-7 for the `if' form.
-  // std::map<std::string, int> m{{"foo", 10}};
-  // std::vector<std::string> keys{"foo", "bar"};
-  // for (auto& key : keys) {
-  //   if (auto p = m.try_emplace(key, 20); !p.second) {
-  //     std::cerr << "Element already registered";
-  //   }
-  // }
+  std::map<std::string, int> m{{"foo", 10}};
+  std::vector<std::string> keys{"foo", "bar"};
+  for (auto& key : keys) {
+    if (auto p = m.try_emplace(key, 20); p.second) {
+      std::cerr << "Emplaced key " << key << "\n";
+    } else {
+      std::cerr << "Element " << key << " already registered\n";
+    }
+  }
 }
